58a1d54280b915c536cea2ed1d8d23c8674294a9
sajid <sajid@jardine-d-84-135.reshall.ksu.edu>
2016-09-26 09:34:55 -0500

    Pom updated with vtd-xml library

---
+++ src/main/java/edu/kstate/so/SOMiner/App.java
@@ -0,0 +1,13 @@
+package edu.kstate.so.SOMiner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
